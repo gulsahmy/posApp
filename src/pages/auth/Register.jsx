af4300cc1436e38870b9ef77ef66c24c884b3db8
gulsah <gmutluayyigit@gmail.com>
2025-02-15 01:20:30 +0300

    register-carousel

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,5 +1,6 @@
-import { Button, Form, Input } from "antd";
-import { Link } from "react-router-dom"
+import { Button, Carousel, Form, Input } from "antd";
+import { Link } from "react-router-dom";
+import AuthCarousel from "../../components/auth/AuthCarousel";
 
 const Register = () => {
   return (
@@ -57,14 +58,51 @@ const Register = () => {
               <Input.Password />
             </Form.Item>
             <Form.Item>
-              <Button type="primary" htmlType="submit" className="w-full" size="large">Kaydol</Button>
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="w-full"
+                size="large"
+              >
+                Kaydol
+              </Button>
             </Form.Item>
           </Form>
           <div className="flex justify-center absolute left-0 bottom-10 w-full">
-            Bir hesabınız var mı? &nbsp; <Link className="text-blue-900" to="/login">Şimdi giriş yap</Link>
+            Bir hesabınız var mı? &nbsp;{" "}
+            <Link className="text-blue-900" to="/login">
+              Şimdi giriş yap
+            </Link>
           </div>
         </div>
-        <div className="xl:w-4/6 min-w-[800px]">right</div>
+        <div className="xl:w-4/6 lg:w-3/5 md:w-1/2 md:flex hidden bg-[#6c63ff] h-full">
+          <div className="w-full h-full flex items-center">
+            <div className="w-full">
+              <Carousel className="!h-full px-6">
+                <AuthCarousel
+                  img="/images/responsive.svg"
+                  title="Responsive"
+                  desc="Tüm Cihaz Boyutlarıyla Uyumluluk"
+                />
+                <AuthCarousel
+                  img="/images/statistic.svg"
+                  title="İstatistikler"
+                  desc="Geniş Tutulan İstatistikler"
+                />
+                <AuthCarousel
+                  img="/images/customer.svg"
+                  title="Müşteri Memnuniyeti"
+                  desc="Deneyim Sonunda Üründen Memnun Müşteriler"
+                />
+                <AuthCarousel
+                  img="/images/admin.svg"
+                  title="Yönetici Paneli"
+                  desc="Tek Yerden Yönetim"
+                />
+              </Carousel>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
