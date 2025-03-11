d828f11e0ed0f88750f70a7e5669f64f020972f8
gulsah <gmutluayyigit@gmail.com>
2025-03-11 23:14:55 +0300

    total

--- src/components/cart/CartTotals.jsx
+++ src/components/cart/CartTotals.jsx
@@ -9,15 +9,15 @@ import { useDispatch } from "react-redux";
 import { deleteCart } from "../../redux/cartSlice";
 
 const CartTotals = () => {
-  const { cartItems } = useSelector((state) => state.cart);
-  const dispatch = useDispatch()
+  const cart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
   return (
     <div className="cart h-full max-h-[calc(100vh_-_90px)] flex flex-col">
       <h2 className="bg-blue-600 text-center py-4 text-white font-bold tracking-wide">
         Sepetteki Ürünler
       </h2>
       <ul className="cart-items px-2 flex flex-col gap-y-3 py-2 overflow-y-auto">
-        {cartItems.map((item) => (
+        {cart.cartItems.map((item) => (
           <li className="cart-item flex justify-between" key={item._id}>
             <div className="flex items-center">
               <img
@@ -28,7 +28,9 @@ const CartTotals = () => {
               />
               <div className="flex flex-col ml-2">
                 <b>{item.title}</b>
-                <span>{item.price}₺ x {item.quantity} </span>
+                <span>
+                  {item.price}₺ x {item.quantity}{" "}
+                </span>
               </div>
             </div>
             <div className="flex items-center gap-x-1">
@@ -53,17 +55,27 @@ const CartTotals = () => {
         <div className="border-t border-b">
           <div className="flex justify-between p-2">
             <b>Ara Toplam</b>
-            <span>99₺</span>
+            <span>{cart.total > 0 ? cart.total.toFixed(2) : 0}₺</span>
           </div>
           <div className="flex justify-between p-2">
-            <b>KDV %8</b>
-            <span className="text-red-700">+7.92₺</span>
+            <b>KDV %{cart.tax}</b>
+            <span className="text-red-700">
+              {(cart.total * cart.tax) / 100 > 0
+                ? `+${((cart.total * cart.tax) / 100).toFixed(2)} `
+                : 0}
+              ₺
+            </span>
           </div>
         </div>
         <div className="border-b mt-4">
           <div className="flex justify-between p-2">
             <b className="text-xl text-green-500">Genel Toplam</b>
-            <span className="text-xl">99₺</span>
+            <span className="text-xl">
+              {cart.total + (cart.total * cart.tax) / 100 > 0
+                ? (cart.total + (cart.total * cart.tax) / 100).toFixed(2)
+                : 0}
+              ₺
+            </span>
           </div>
         </div>
         <div className="py-4 px-2">
