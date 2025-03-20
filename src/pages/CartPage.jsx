79e87286db580be5da418e5f37ae509db8f14082
gulsah <gmutluayyigit@gmail.com>
2025-03-20 15:26:39 +0300

    sepet-add-del

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,12 +1,16 @@
 import Header from "../components/header/Header";
-import { Button, Card, Table } from "antd";
+import { Button, Card, Popconfirm, Table, message } from "antd";
 import { useState } from "react";
 import CreateBill from "../components/cart/CreateBill";
+import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
+import { MinusCircleOutlined, PlusCircleOutlined } from "@ant-design/icons";
+import { deleteCart, decrease, increase } from "../redux/cartSlice";
 
 const CartPage = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const cart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
 
   const columns = [
     {
@@ -15,8 +19,8 @@ const CartPage = () => {
       key: "img",
       width: "125px",
       render: (text) => {
-        return (<img src={text} alt=""className="w-full h-20 object-cover"/>)
-      }
+        return <img src={text} alt="" className="w-full h-20 object-cover" />;
+      },
     },
     {
       title: "Ürün Adı",
@@ -32,6 +36,78 @@ const CartPage = () => {
       title: "Ürün Fiyatı",
       dataIndex: "price",
       key: "price",
+      render: (text) => {
+        return <span> {text.toFixed(2)}₺ </span>;
+      },
+    },
+    {
+      title: "Ürün adedi",
+      dataIndex: "quantity",
+      key: "quantity",
+      render: (text, record) => {
+        return (
+          <div className="flex items-center">
+            <Button
+              type="primary"
+              size="small"
+              className="w-full flex items-center justify-center rounded-full"
+              icon={<PlusCircleOutlined />}
+              onClick={() => dispatch(increase(record))}
+            />
+            <span className="font-bold w-6 inline-block text-center">
+              {record.quantity}
+            </span>
+            <Button
+              type="primary"
+              size="small"
+              className="w-full flex items-center justify-center rounded-full"
+              icon={<MinusCircleOutlined />}
+              onClick={() => {
+                if (record.quantity === 1) {
+                  if (window.confirm("Ürün silinsin mi?")) {
+                    dispatch(decrease(record));
+                    message.success("Ürün sepetten silindi.");
+                  }
+                }
+                if (record.quantity > 1) {
+                  dispatch(decrease(record));
+                }
+              }}
+            />
+          </div>
+        );
+      },
+    },
+    {
+      title: "Toplam Fiyat",
+      render: (text, record) => {
+        return <span> {(record.quantity * record.price).toFixed(2)}₺ </span>;
+      },
+    },
+    {
+      title: "Actions",
+      render: (_, record) => {
+        return (
+          <Popconfirm 
+          title="Silmek için emin misiniz?" 
+          onConfirm={() => {
+              dispatch(deleteCart(record));
+              message.success("Ürün sepetten silindi.");
+            }}
+            okText="Evet"
+            cancelText="Hayır"
+          >
+               <Button
+            type="link"
+            danger
+           
+          >
+            {" "}
+            Sil{" "}
+          </Button>
+          </Popconfirm>
+        );
+      },
     },
   ];
 
